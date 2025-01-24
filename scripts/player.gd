2d574d59e39abbf201cfbe3fe3752a2de4ae7d74
Tomáš Polák <polak.tomas01@gmail.com>
2025-01-24 22:46:00 +0100

    add interaction system

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,13 +5,21 @@ extends CharacterBody3D
 @export var jump_strength: float = 8.0
 @export var gravity: float = -20.0
 
+var interactable_object: PhysicsBody3D = null
+
 # Internal variables
 var pitch: float = 0.0
+
+# Capture mouse on startup
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # Called every frame
 func _process(delta: float) -> void:
 	handle_movement(delta)
 	handle_look()
+	check_interaction()
+	handle_interaction()
 
 # Handle player movement
 func handle_movement(delta: float) -> void:
@@ -40,6 +48,27 @@ func handle_look() -> void:
 	$Camera3D.rotation_degrees.x = pitch
 	rotation_degrees.y -= mouse_delta.x
 
-# Capture mouse on startup
-func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+func check_interaction() -> void:
+	# Shoot ray from the center of the screen (considering the camera and it's rotation)
+	var ray_from: Vector3 = $Camera3D.global_transform.origin
+	var ray_dir: Vector3 = $Camera3D.global_transform.basis.z * -1
+	var ray_to: Vector3 = ray_from + ray_dir * 1.8
+
+	# Cast the ray
+	var space_state = get_world_3d().direct_space_state
+	var result = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(ray_from, ray_to))
+	if result:
+		var collider = result.collider as PhysicsBody3D
+		if collider.is_in_group("interactable"):
+			interactable_object = collider
+			$Control/InteractLabel.visible = true
+	else:
+		interactable_object = null
+		$Control/InteractLabel.visible = false
+
+func handle_interaction() -> void:
+	if interactable_object != null and Input.is_action_just_pressed("interact"):
+		if interactable_object.has_method("interact"):
+			interactable_object.interact()
+		else:
+			print_debug("missing interact method")
