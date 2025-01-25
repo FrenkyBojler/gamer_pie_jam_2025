1a1c8836ce89bff55fc779521fed2ff8c5e8adcf
Tomáš Polák <polak.tomas01@gmail.com>
2025-01-25 20:38:31 +0100

    update PC ui

--- scripts/pc_ui.gd
+++ scripts/pc_ui.gd
@@ -1,5 +1,23 @@
 extends Control
 
+@export
+var user_name: Label
+@export
+var user_picture: TextureRect
+
+@export
+var points_label_1: Label
+@export
+var progress_bar_1: ProgressBar
+
+@export
+var points_label_2: Label
+@export
+var progress_bar_2: ProgressBar
+
+@export
+var posts_list: VBoxContainer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
