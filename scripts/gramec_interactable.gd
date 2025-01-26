502bb5d602719abdca54f4d217028a011b93b21b
Tomáš Polák <polak.tomas01@gmail.com>
2025-01-26 09:44:16 +0100

    add gramec button

--- scripts/gramec_interactable.gd
+++ scripts/gramec_interactable.gd
@@ -5,6 +5,7 @@ var anim_player: AnimationPlayer
 
 var is_playing := true
 var can_interact := false
+var can_interact_with_button := false
 
 var last_mouse_pos: Vector2
 
@@ -21,6 +22,13 @@ func _process(delta: float) -> void:
 		return
 	if Input.is_action_just_pressed("place_or_pickup"):
 		_spin_handle()
+		_push_button()
+
+func _push_button() -> void:
+	if can_interact_with_button:
+		anim_player.play("button")
+		await get_tree().create_timer(1).timeout
+		anim_player.play("GramophoneRecordSpin")
 
 func _spin_handle() -> void:
 	if can_interact:
@@ -32,4 +40,12 @@ func _on_static_body_3d_mouse_entered() -> void:
 
 func _on_static_body_3d_mouse_exited() -> void:
 	can_interact = false
+	player.hide_generic_label()
+
+func _on_button_col_mouse_entered() -> void:
+	can_interact_with_button = true
+	player.show_generic_label("Click to push", last_mouse_pos)
+
+func _on_button_col_mouse_exited() -> void:
+	can_interact_with_button = false
 	player.hide_generic_label()
