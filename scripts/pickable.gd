1129fd00581f3279ee4020416c132023227594ac
Tomas Polak <tomas@laposa.ie>
2025-02-02 21:59:08 +0100

    outline pickable objects

--- scripts/pickable.gd
+++ scripts/pickable.gd
@@ -3,6 +3,7 @@ extends StaticBody3D
 class_name Pickable
 
 const placeholder_mat = preload("res://assets/materials/placeholder_mat.tres")
+const outline_mat = preload("res://assets/materials/new_outline_mat.tres")
 
 @export
 var id: String = ""
@@ -18,7 +19,7 @@ var is_picked := false
 
 var current_interactable: Interactable = null
 
-var visual_instance_3D: VisualInstance3D = null
+var visual_instance_3D: MeshInstance3D = null
 
 signal placed
 
@@ -41,13 +42,10 @@ func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 	
-	if is_placeholder:
-		for child in get_children():
-			if child is MeshInstance3D:
-				var mesh_instance: MeshInstance3D = child
-				mesh_instance.material_override = placeholder_mat
-
 	find_visual_instance_3D()
+
+	if is_placeholder:
+		visual_instance_3D.material_override = placeholder_mat
 
 func find_visual_instance_3D() -> void:
 	for child in get_children():
@@ -57,10 +55,7 @@ func find_visual_instance_3D() -> void:
 
 func init_pickable() -> void:
 	if is_placeholder:
-		for child in get_children():
-			if child is MeshInstance3D:
-				var mesh_instance: MeshInstance3D = child
-				mesh_instance.material_override = placeholder_mat
+		visual_instance_3D.material_override = placeholder_mat
 
 func _on_mouse_entered() -> void:
 	if is_picked:
@@ -70,20 +65,18 @@ func _on_mouse_entered() -> void:
 	elif is_placeholder and player.picked_object == null or (player.picked_object != null and player.picked_object.id != id):
 		player.show_missing_object_label(current_mouse_pos)
 	elif not is_placeholder and player.picked_object == null and current_interactable.in_interaction:
+		show_outline()
 		player.show_pickup_object_label(current_mouse_pos, self)
 
 func _on_mouse_exited() -> void:
 	if is_picked:
 		return
 	player.hide_all_labels()
+	hide_outline()
 	
 func pickup() -> void:
 	is_placeholder = true
-	print(visual_instance_3D.name)
-	for child in get_children():
-		if child is MeshInstance3D:
-			var mesh_instance: MeshInstance3D = child
-			mesh_instance.material_override = placeholder_mat
+	visual_instance_3D.material_override = placeholder_mat
 
 func place() -> void:
 	placed.emit()
@@ -91,10 +84,15 @@ func place() -> void:
 	is_placeholder = false
 	visible = true
 	switch_to_rendering_layer(1)
-	for child in get_children():
-		if child is MeshInstance3D:
-			var mesh_instance: MeshInstance3D = child
-			mesh_instance.material_override = null
+	visual_instance_3D.material_override = null
 
 func switch_to_rendering_layer(layer: int) -> void:
 	visual_instance_3D.layers = layer
+
+func show_outline() -> void:
+	var current_mat = visual_instance_3D.get_active_material(0).duplicate()
+	current_mat.next_pass = outline_mat
+	visual_instance_3D.set_surface_override_material(0, current_mat)
+
+func hide_outline() -> void:
+	visual_instance_3D.set_surface_override_material(0, null)
